6a491492441c0bfa30a5e94c2cbb6fe972462813
KSH701 <rkrk7636@naver.com>
2026-03-10 11:19:33 +0900

    Style: AskChat 컴포넌트 스타일링

--- src/features/ask/AskChat.tsx
+++ src/features/ask/AskChat.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import { ChatInput } from "../../components/common";
+import { ChatInput, ChatLoading } from "../../components/common";
 
 import { useAskMessageStream, useAskStream } from "@/hooks/mutations";
 import { Spinner } from "../../components/ui/spinner/spinner";
@@ -9,17 +9,20 @@ import { CLOSENESS_OPTIONS, Step, STEP_QUESTIONS, STEPS } from "@/constants";
 import { Button } from "@/components/ui/button/button";
 import ChatQuickActions from "./ChatQuickActions";
 import { useVoiceChat } from "@/hooks/custom";
+import { BulbIcon, ChevronDownIcon } from "@/assets/svgr";
+import { ChevronUp } from "lucide-react";
+import { CominSoonModal } from "@/components/modal";
 
 export default function AskChat() {
   const [step, setStep] = useState<Step>("askTarget");
   const [message, setMessage] = useState("");
+  const [open, setOpen] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false);
   const [askTarget, setAskTarget] = useState("");
   const [closeness, setCloseness] = useState("");
   const [situation, setSituation] = useState("");
   const bottomRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [message]);
+
   const {
     mutate: createAsk,
     isPending,
@@ -41,11 +44,12 @@ export default function AskChat() {
   const currentStepIdx = STEPS.indexOf(step);
   useEffect(() => {
     if (micState === "recorded" && sttText) {
-      console.log(sttText, micState);
       setMessage(sttText);
     }
   }, [sttText, micState]);
-
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [aiMessage, turns]);
   const handleSendTarget = () => {
     if (!message.trim()) return;
     setAskTarget(message.trim());
@@ -79,9 +83,13 @@ export default function AskChat() {
     }
     setMessage("");
   };
+  const handleInsight = () => {
+    setOpen((prev) => !prev);
+  };
   return (
     <div className="flex w-full flex-1 flex-col">
-      <div className="flex flex-1 flex-col">
+      <CominSoonModal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
+      <div className="flex flex-1 flex-col pb-2">
         {/* 타겟 */}
         <span className="text-xl font-semibold">
           {STEP_QUESTIONS.askTarget}
@@ -160,16 +168,40 @@ export default function AskChat() {
 
         {/* 스트리밍 결과 */}
         {aiMessage && (
-          <div className="mt-5 flex flex-col gap-3">
-            {approachTip && (
-              <p className="text-sm text-gray-600">{approachTip}</p>
-            )}
-            <div className="rounded-b-xl rounded-tr-xl border border-gray-300 bg-white p-4">
-              <p className="text-sm">{aiMessage}</p>
+          <div className="mb-1 flex flex-col gap-2">
+            <div className="flex flex-col">
+              <span className="text-xl font-semibold">
+                Here is the best way to say it
+              </span>
+              <span className="text-gray-600">{approachTip}</span>
             </div>
-            {culturalInsight && (
-              <p className="text-sm text-gray-500">{culturalInsight}</p>
-            )}
+            <div className="flex flex-col gap-2">
+              <div className="max-w-61 mb-5 flex flex-col gap-2 rounded-b-xl rounded-tr-xl border border-gray-300 bg-white p-4">
+                <p className="my-1 text-sm">{aiMessage}</p>
+              </div>
+              <div className="flex flex-col gap-1 border-b border-gray-400 pb-2 text-sm">
+                <div className="flex gap-1 text-blue-600">
+                  <BulbIcon size={14} /> Cultural Insights
+                </div>
+                {open ? (
+                  <>
+                    {culturalInsight}
+                    <div className="flex items-center justify-center">
+                      <ChevronDownIcon onClick={handleInsight} />
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className="w-full min-w-0">
+                      <p className="flex-1 truncate">{culturalInsight}</p>
+                      <div className="flex items-center justify-center">
+                        <ChevronUp onClick={handleInsight} />
+                      </div>
+                    </div>
+                  </>
+                )}
+              </div>
+            </div>
           </div>
         )}
         {/* 이후 스트리밍 메세지 */}
@@ -185,7 +217,7 @@ export default function AskChat() {
                 {turn.approachTip && (
                   <p className="text-sm text-gray-600">{turn.approachTip}</p>
                 )}
-                <div className="rounded-b-xl rounded-tr-xl border border-gray-300 bg-white p-4">
+                <div className="w-61 rounded-b-xl rounded-tr-xl border border-gray-300 bg-white p-4">
                   <p className="text-sm">{turn.aiMessage}</p>
                 </div>
                 {turn.culturalInsight && (
@@ -197,11 +229,15 @@ export default function AskChat() {
             )}
           </div>
         ))}
+        {isAIResponding && <ChatLoading />}
+        <div ref={bottomRef} />
       </div>
 
       {/* 하단 고정 ChatInput */}
       <div className="sticky bottom-0 flex flex-col pb-5 backdrop-blur-md">
-        {aiMessage && <ChatQuickActions />}
+        {aiMessage && (
+          <ChatQuickActions onOpenModal={() => setModalOpen(true)} />
+        )}
         {step === "askTarget" && (
           <ChatInput
             message={message}
