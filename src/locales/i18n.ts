2faeed04accca52b6dab29f2652355896ae02221
KSH701 <rkrk7636@naver.com>
2026-05-06 15:12:52 +0900

    chore: i18n 패키지 추가

---
+++ src/locales/i18n.ts
@@ -0,0 +1,19 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+import en from './namespace/en.json'
+import ko from './namespace/ko.json'
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    ko: { translation: ko },
+  },
+  lng: 'ko',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+})
+
+export default i18n
