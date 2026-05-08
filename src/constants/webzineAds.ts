1eef70a76c70e4edd062c6b6e3f35f5b06eb5fc3
KSH701 <rkrk7636@naver.com>
2026-05-08 16:12:17 +0900

    feat: 웹진 컨텐츠 추가

---
+++ src/constants/webzineAds.ts
@@ -0,0 +1,10 @@
+export const WEBZINE_ADS = [
+  {
+    i18nKey: "webzine.landing",
+    url: "https://noonchiapp.vercel.app/",
+  },
+  {
+    i18nKey: "webzine.taekwondo",
+    url: "",
+  },
+] as const;
